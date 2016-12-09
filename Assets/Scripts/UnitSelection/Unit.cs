306dbe7bbf7b6351ac9b1e27054d6eee0244e585
Raul Perez <zerepangel@gmail.com>
2016-12-09 11:19:40 +0100

    Unit script to be selectable

--- Assets/Scripts/UnitSelection/Unit.cs
+++ Assets/Scripts/UnitSelection/Unit.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public bool selected = false;
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (GetComponent<Renderer>().isVisible && Input.GetMouseButtonUp(0))
+        {
+            Vector3 camPos = Camera.main.WorldToScreenPoint(transform.position);
+            camPos.y = CameraOperator.ScreenToRectTransform(camPos.y);
+            selected = CameraOperator.selection.Contains(camPos);
+        }
+
+        if (selected)
+            GetComponent<Renderer>().material.color = Color.red;
+        else
+            GetComponent<Renderer>().material.color = Color.white;
 	}
 }
